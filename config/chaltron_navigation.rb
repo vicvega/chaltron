2245a8ddfb0956e5aecb6ede5c522025b6fe73b5
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-09-05 12:15:38 +0200

    fix nil fullname issue

--- config/chaltron_navigation.rb
+++ config/chaltron_navigation.rb
@@ -4,7 +4,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.dom_class = 'pull-right'
 
     if signed_in?
-      primary.item :logged, current_user.fullname.html_safe, nil do |user|
+      primary.item :logged, current_user.display_name.html_safe, nil do |user|
         user.item :logout, { icon: 'fa fa-fw fa-sign-out', text: 'Logout' },
                   destroy_user_session_url, method: :delete
       end
