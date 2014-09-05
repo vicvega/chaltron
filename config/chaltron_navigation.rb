0d2a64653d2b018c7894a2a65fe7e97368b8eee0
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-09-05 11:00:07 +0200

    fix styling

--- config/chaltron_navigation.rb
+++ config/chaltron_navigation.rb
@@ -5,11 +5,11 @@ SimpleNavigation::Configuration.run do |navigation|
 
     if signed_in?
       primary.item :logged, current_user.fullname.html_safe, nil do |user|
-        user.item :logout, {icon: 'fa fa-fw fa-sign-out', text: 'Logout'},
-                           destroy_user_session_url, method: :delete
+        user.item :logout, { icon: 'fa fa-fw fa-sign-out', text: 'Logout' },
+                  destroy_user_session_url, method: :delete
       end
     else
-      primary.item :login, {icon: 'fa fa-fw fa-sign-in', text: 'Login'}, new_user_session_url
+      primary.item :login, { icon: 'fa fa-fw fa-sign-in', text: 'Login' }, new_user_session_url
     end
   end
 end
