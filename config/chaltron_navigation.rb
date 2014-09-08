938d17eb9c06a00ed47a023e59f52b2a9833744c
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-09-08 18:23:45 +0200

    add template for user index view

--- config/chaltron_navigation.rb
+++ config/chaltron_navigation.rb
@@ -4,6 +4,15 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.dom_class = 'pull-right'
 
     if signed_in?
+      primary.item :admin,
+        { icon: 'fa fa-fw fa-cogs',
+          text: I18n.t('chaltron.menu.admin').titleize
+        }, nil do |admin|
+        admin.item :users,
+        { icon: 'fa fa-fw fa-users',
+          text: I18n.t('chaltron.menu.users').titleize
+        }, users_url
+      end
       primary.item :logged, current_user.display_name.html_safe, nil do |user|
         user.item :logout, { icon: 'fa fa-fw fa-sign-out', text: 'Logout' },
                   destroy_user_session_url, method: :delete
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'chaltron/omniauth_callbacks' }
+  resources :users, only: [:index], controller: 'chaltron/users'
 end
