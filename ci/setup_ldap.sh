c89eed8db4b09ac92624224f314213c2fee789eb
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-09-09 18:01:09 +0200

    workin on travis ci... to be continued 3

---
+++ ci/setup_ldap.sh
@@ -0,0 +1,17 @@
+cat > /tmp/slapd.debconf << EOF
+slapd slapd/password1 string Secret007!
+slapd slapd/password2 string Secret007!
+EOF
+
+/usr/bin/debconf-set-selections /tmp/slapd.debconf
+
+apt-get update
+apt-get install slapd ldap-utils sudo-ldap -y
+
+cat > /etc/ldap/ldap.conf << EOF
+BASE dc=azkaban,dc=co,dc=uk
+BINDDN cn=admin,dc=azkaban,dc=co,dc=uk
+URI ldap://localhost
+EOF
+
+ldapadd -x -w Secret007! -f ./ci/ldap/entry.ldif -D 'cn=admin,dc=azkaban,dc=co,dc=uk'
