afe69375ad98175a2314e378fe22a19062e34a6e
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-10-09 18:06:52 +0200

    first draft of user view

--- app/controllers/chaltron/users_controller.rb
+++ app/controllers/chaltron/users_controller.rb
@@ -1,5 +1,8 @@
 class Chaltron::UsersController < ApplicationController
   def index
     @users = User.all
+    # apply provider filter
+    @users = @users.where(provider: nil) if params[:provider] == 'local'
+    @users = @users.where(provider: :ldap) if params[:provider] == 'ldap'
   end
 end
