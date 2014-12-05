eb48a16465e8f2317c4f299a4d72abc75a730511
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-12-05 14:21:51 +0100

    reduce code complexity

--- lib/generators/chaltron/install_generator.rb
+++ lib/generators/chaltron/install_generator.rb
@@ -9,26 +9,7 @@ module Chaltron
     class_option :interactive, type: :boolean, desc: 'Run interactive configuration.', default: true
 
     def disclaimer
-      @banner = Chaltron::Banner.new
-      message = "\tBe proud! You are running\n" +
-      set_color(@banner.sample, :blue, true) + "\n"
-      say message
-
-      message = "\taka " +
-      set_color('Muffaster reloaded', :red)
-      say message
-
-      message = 'Welcome to ' +
-      set_color('L', :blue, true) +
-      'igth ' +
-      set_color('S', :blue, true) +
-      'peed ' +
-      set_color('A', :blue, true) +
-      'pplication ' +
-      set_color('D', :blue, true) +
-      'evelopment!'
-      say message
-
+      print_banner
       if options.interactive?
         exit unless yes?('Are you sure you want to continue? [yes/NO]')
       end
@@ -147,5 +128,34 @@ RUBY
     def setup_authorization
       copy_file 'app/models/ability.rb'
     end
- end
+
+    private
+
+    def print_banner
+      banner = Chaltron::Banner.new
+    message = <<-TXT
+
+    Be proud! You are running
+
+#{set_color(banner.sample, :blue, true)}
+    aka #{set_color('Muffaster reloaded', :red)}
+
+    TXT
+      say message
+      print_welcome
+    end
+
+    def print_welcome
+      message = 'Welcome to ' +
+      set_color('L', :blue, true) +
+      'igth ' +
+      set_color('S', :blue, true) +
+      'peed ' +
+      set_color('A', :blue, true) +
+      'pplication ' +
+      set_color('D', :blue, true) +
+      'evelopment!'
+      say message
+    end
+  end
 end
