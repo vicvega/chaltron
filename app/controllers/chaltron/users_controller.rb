2d2def474972a22cff0690e8c8079ed8b3f5ce66
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2014-12-24 15:58:05 +0100

    better use of user filters without merging params itself

--- app/controllers/chaltron/users_controller.rb
+++ app/controllers/chaltron/users_controller.rb
@@ -3,11 +3,13 @@ class Chaltron::UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
+    @filters = params[:filters] || {}
+
     # apply provider filter
-    @users = @users.where(provider: nil) if params[:provider] == 'local'
-    @users = @users.where(provider: :ldap) if params[:provider] == 'ldap'
+    @users = @users.where(provider: nil) if @filters[:provider] == 'local'
+    @users = @users.where(provider: :ldap) if @filters[:provider] == 'ldap'
     # apply activity filter
-    @users = @users.where(sign_in_count: 0) if params[:activity] == 'no_login'
+    @users = @users.where(sign_in_count: 0) if @filters[:activity] == 'no_login'
   end
 
   def show
