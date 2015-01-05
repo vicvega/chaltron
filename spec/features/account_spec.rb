5fa33c4ce7aabbac725601cdeecb48a8be36dae5
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-05 17:07:06 +0100

    add test for changing password

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -7,7 +7,6 @@ describe User do
 
     it 'edits account data' do
       visit self_show_path
-
       expect(page).to have_content user.fullname
 
       click_link 'Edit account data'
@@ -19,6 +18,25 @@ describe User do
       expect(page).to have_content I18n.t('chaltron.users.self_updated')
       expect(page).to have_content fullname
     end
+
+    it 'changes password' do
+      visit self_show_path
+      expect(page).to have_content user.fullname
+
+      click_link 'Edit account data'
+
+      password = 'password.1'
+      fill_in 'user_password', with: password
+      fill_in 'user_password_confirmation', with: password
+      click_button 'Edit account data'
+
+      expect(page).to have_content I18n.t('chaltron.users.self_updated')
+
+      logout
+      login_with user.username, password
+      expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    end
+
 
   end
 end
