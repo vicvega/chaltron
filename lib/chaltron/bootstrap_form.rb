123c053caf4e8b5b19a61fd94b98d4cb019468eb
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-07 19:12:08 +0100

    disable role update on current user

--- lib/chaltron/bootstrap_form.rb
+++ lib/chaltron/bootstrap_form.rb
@@ -1,11 +1,11 @@
 class BootstrapForm::FormBuilder
 
-  def role_select
+  def role_select(opts = {})
     collection = Chaltron.roles.map {|role| [ role, I18n.translate("roles.#{role}") ] }
     html = inputs_collection(:roles, collection, :first, :last, checked: @object.roles) do |name, value, options|
       options[:multiple] = true
       options[:inline] = true
-      check_box(name, options, value, nil)
+      check_box(name, options.merge(opts), value, nil)
     end
     hidden_field(:roles,{value: '', multiple: true}).concat(html)
   end
