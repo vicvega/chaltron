728242dd17726e788a13e8c1d9d8147cff5e3065
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-07 19:00:28 +0100

    add label to current user in users list

--- app/helpers/chaltron_helper.rb
+++ app/helpers/chaltron_helper.rb
@@ -32,4 +32,23 @@ module ChaltronHelper
       icon(ic) + '&nbsp;'.html_safe + text
     end
   end
+
+  #
+  # Badge and label helpers
+  #
+  def badge(*args)
+    badge_label(:badge, *args)
+  end
+
+  def tag_label(*args)
+    badge_label(:label, *args)
+  end
+
+  private
+
+  def badge_label(what, value, type = nil)
+    klass = [what]
+    klass << "#{what}-#{type}" if type.present?
+    content_tag :span, value, :class => "#{klass.join(' ')}"
+  end
 end
