4375068cfef85f1f160d8366e2099965bb4fe028
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-08 18:56:05 +0100

    disabled checkbox are not submitted... need to work out another solution to avoid self updating roles

--- lib/chaltron/bootstrap_form.rb
+++ lib/chaltron/bootstrap_form.rb
@@ -1,11 +1,11 @@
 class BootstrapForm::FormBuilder
 
-  def role_select(opts = {})
+  def role_select
     collection = Chaltron.roles.map {|role| [ role, I18n.translate("roles.#{role}") ] }
     html = inputs_collection(:roles, collection, :first, :last, checked: @object.roles) do |name, value, options|
       options[:multiple] = true
       options[:inline] = true
-      check_box(name, options.merge(opts), value, nil)
+      check_box(name, options, value, nil)
     end
     hidden_field(:roles,{value: '', multiple: true}).concat(html)
   end
