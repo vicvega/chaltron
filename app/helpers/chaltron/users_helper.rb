2d0c2b468f5d139d1a2de89a8989fb2b4eba0b78
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-08 16:49:59 +0100

    change color to it_s_you badge

--- app/helpers/chaltron/users_helper.rb
+++ app/helpers/chaltron/users_helper.rb
@@ -2,7 +2,7 @@ module Chaltron::UsersHelper
   def display_username(user)
     if user == current_user
       link_to(user.username, user) + "&nbsp".html_safe +
-        tag_label(I18n.t('chaltron.users.you'), :danger)
+        tag_label(I18n.t('chaltron.users.it_s_you'), :success)
     else
       link_to user.username, user
     end
