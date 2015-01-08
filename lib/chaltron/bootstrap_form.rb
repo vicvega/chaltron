a39386a9d29e44c57eeb55afd879ba85f687aee1
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-08 19:17:18 +0100

    current user cannot remove his own user_amin role

--- lib/chaltron/bootstrap_form.rb
+++ lib/chaltron/bootstrap_form.rb
@@ -1,13 +1,20 @@
 class BootstrapForm::FormBuilder
 
-  def role_select
+  def role_select(opts={})
     collection = Chaltron.roles.map {|role| [ role, I18n.translate("roles.#{role}") ] }
     html = inputs_collection(:roles, collection, :first, :last, checked: @object.roles) do |name, value, options|
       options[:multiple] = true
       options[:inline] = true
+
+      if value == opts[:disabled]
+        options[:disabled] = true
+        options[:checked] = true
+      end
       check_box(name, options, value, nil)
     end
-    hidden_field(:roles,{value: '', multiple: true}).concat(html)
+
+    hidden = opts[:disabled] || ''
+    hidden_field(:roles,{value: hidden, multiple: true}).concat(html)
   end
 
 end
