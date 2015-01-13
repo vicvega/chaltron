a70617589bcdc4845d0a35cc0d01156f71acf102
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-13 18:12:16 +0100

    add badge on ldap entry list

--- app/helpers/chaltron/users_helper.rb
+++ app/helpers/chaltron/users_helper.rb
@@ -1,10 +1,29 @@
 module Chaltron::UsersHelper
   def display_username(user)
     if user == current_user
-      link_to(user.username, user) + "&nbsp".html_safe +
+      link_to(user.username, user) + '&nbsp'.html_safe +
         tag_label(I18n.t('chaltron.users.it_s_you'), :success)
     else
       link_to user.username, user
     end
   end
+
+  def display_ldap_entry_username(entry)
+    if current_user.username == entry.username
+      (entry.username + '&nbsp').html_safe +
+        tag_label(I18n.t('chaltron.users.it_s_you'), :success)
+    else
+      entry.username
+    end
+  end
+
+  def display_ldap_entry_email(entry)
+    mail = entry.email
+    if mail.blank?
+      tag_label(I18n.t('chaltron.users.missing_field'), :danger)
+    else
+      mail
+    end
+  end
+
 end
