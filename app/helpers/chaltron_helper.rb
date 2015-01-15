0ec7a0642cdc6f8210da14fcee71de77c69d8cb9
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-15 11:06:55 +0100

    remove unuseful code

--- app/helpers/chaltron_helper.rb
+++ app/helpers/chaltron_helper.rb
@@ -17,7 +17,7 @@ module ChaltronHelper
   end
 
   def flash_message(message, type)
-    content_tag(:div, message, class: "alert #{bootstrap_class_for(type)} alert_link") do
+    content_tag(:div, message, class: "alert #{bootstrap_class_for(type)}") do
       content_tag(:strong, I18n.t("chaltron.flash.#{type}").capitalize + ': ') +
       message
     end
