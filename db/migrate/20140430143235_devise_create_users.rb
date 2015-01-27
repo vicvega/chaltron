d5f7cfaeb207200507bd07ad64638a0f6b92ce0f
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-27 18:29:08 +0100

    fixed deprecation warning on migration

--- db/migrate/20140430143235_devise_create_users.rb
+++ db/migrate/20140430143235_devise_create_users.rb
@@ -30,7 +30,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.timestamps
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141205095036) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20141205095036) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "fullname"
     t.string   "provider"
