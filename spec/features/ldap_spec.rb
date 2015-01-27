6e2954256508cdc9d7739934ed34333d63ae3b4d
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-01-27 09:51:38 +0100

    minor tests redesign

--- spec/features/ldap_spec.rb
+++ spec/features/ldap_spec.rb
@@ -7,59 +7,55 @@ describe User do
     let(:user_admin) { create :user_admin }
     before { login_with user_admin.username, user_admin.password }
     subject { page }
-    context 'creates LDAP user by uid' do
-      it 'succeeds' do
-        visit users_path
 
-        click_button I18n.t('chaltron.users.index.new_user')
-        click_link I18n.t('chaltron.users.index.new_ldap_user')
-        fill_in 'userid', with: 'sirius'
-        click_button I18n.t('chaltron.ldap.search.submit_text')
+    it 'creates LDAP user by uid' do
+      visit users_path
 
-        is_expected.to have_content 'sirius'
+      click_button I18n.t('chaltron.users.index.new_user')
+      click_link I18n.t('chaltron.users.index.new_ldap_user')
+      fill_in 'userid', with: 'sirius'
+      click_button I18n.t('chaltron.ldap.search.submit_text')
 
-        find('table#ldap_create tr', text: 'sirius').click
-        check :user_roles_admin
-        check :user_roles_user_admin
-        click_button I18n.t('chaltron.ldap.multi_new.submit_text')
+      is_expected.to have_content 'sirius'
 
-        is_expected.to have_css 'li.list-group-item-success'
-        is_expected.to have_content 'Sirius Black'
-        is_expected.not_to have_css 'div.panel-danger'
+      find('table#ldap_create tr', text: 'sirius').click
+      check :user_roles_admin
+      check :user_roles_user_admin
+      click_button I18n.t('chaltron.ldap.multi_new.submit_text')
 
-        expect(User.find_by(username: 'sirius').is? :admin).to be_truthy
-        expect(User.find_by(username: 'sirius').is? :user_admin).to be_truthy
-      end
+      is_expected.to have_css 'li.list-group-item-success'
+      is_expected.to have_content 'Sirius Black'
+      is_expected.not_to have_css 'div.panel-danger'
+
+      expect(User.find_by(username: 'sirius').is? :admin).to be_truthy
+      expect(User.find_by(username: 'sirius').is? :user_admin).to be_truthy
     end
 
-    context 'creates LDAP user by name' do
-      it 'succeeds' do
-        visit users_path
+    it 'creates LDAP users by name' do
+      visit users_path
 
-        click_button I18n.t('chaltron.users.index.new_user')
-        click_link I18n.t('chaltron.users.index.new_ldap_user')
-        fill_in 'fullname', with: 'b'
-        click_button I18n.t('chaltron.ldap.search.submit_text')
+      click_button I18n.t('chaltron.users.index.new_user')
+      click_link I18n.t('chaltron.users.index.new_ldap_user')
+      fill_in 'fullname', with: 'b'
+      click_button I18n.t('chaltron.ldap.search.submit_text')
 
-        is_expected.to have_content 'sirius'
-        is_expected.to have_content 'barty'
+      is_expected.to have_content 'sirius'
+      is_expected.to have_content 'barty'
 
-        find('table#ldap_create tr', text: 'sirius').click
-        find('table#ldap_create tr', text: 'barty').click
+      find('table#ldap_create tr', text: 'sirius').click
+      find('table#ldap_create tr', text: 'barty').click
 
-        check :user_roles_admin
-        click_button I18n.t('chaltron.ldap.multi_new.submit_text')
+      check :user_roles_admin
+      click_button I18n.t('chaltron.ldap.multi_new.submit_text')
 
-        is_expected.to have_css 'li.list-group-item-success'
-        is_expected.to have_content 'Bartemius Crouch'
-        is_expected.to have_content 'Sirius Black'
-        is_expected.not_to have_css 'div.panel-danger'
+      is_expected.to have_css 'li.list-group-item-success'
+      is_expected.to have_content 'Bartemius Crouch'
+      is_expected.to have_content 'Sirius Black'
+      is_expected.not_to have_css 'div.panel-danger'
 
-        expect(User.find_by(username: 'sirius').is? :admin).to be_truthy
-        expect(User.find_by(username: 'barty').is? :admin).to be_truthy
-      end
+      expect(User.find_by(username: 'sirius').is? :admin).to be_truthy
+      expect(User.find_by(username: 'barty').is? :admin).to be_truthy
     end
-
 
  end
 
