8bded3df56d23430e98e05fc8ad7200387427d5c
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-02-02 12:10:49 +0100

    remove old comments

--- app/controllers/chaltron/logs_controller.rb
+++ app/controllers/chaltron/logs_controller.rb
@@ -7,12 +7,6 @@ class Chaltron::LogsController < ApplicationController
   def index
     @filters = params[:filters] || {}
 
-
-#    @users = @users.where(provider: nil) if @filters[:provider] == 'local'
-#    @users = @users.where(provider: :ldap) if @filters[:provider] == 'ldap'
-#    # apply activity filter
-#    @users = @users.where(sign_in_count: 0) if @filters[:activity] == 'no_login'
-
     @logs = @logs.accessible_by(current_ability)
     # apply severity filter
     @logs = @logs.where(severity: @filters[:severity]) if @filters[:severity].present?
