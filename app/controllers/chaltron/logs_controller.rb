5c5e215dd588953d10a10390082b7895076f3d56
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-02-04 12:25:00 +0100

    redesing log view without filters

--- app/controllers/chaltron/logs_controller.rb
+++ app/controllers/chaltron/logs_controller.rb
@@ -1,17 +1,11 @@
 class Chaltron::LogsController < ApplicationController
   before_action :authenticate_user!
-  load_and_authorize_resource except: [:self_show, :self_edit, :self_update]
+  load_and_authorize_resource
 
   respond_to :html
 
   def index
-    @filters = params[:filters] || {}
-
     @logs = @logs.accessible_by(current_ability)
-    # apply severity filter
-    severity = @filters[:severity]
-    @logs = @logs.where(severity: severity) if Log::Severities.include? severity
-
   end
 
   def show
--- app/helpers/chaltron/logs_helper.rb
+++ app/helpers/chaltron/logs_helper.rb
@@ -1,12 +1,20 @@
 module Chaltron::LogsHelper
   def display_panel(log, &block)
-    klass = 'panel panel-'
-    klass += case log.severity.to_sym
-      when :info  then 'primary'
-      when :error then 'danger'
-      when :debug then 'info'
+    klass = "panel panel-#{bootstrap_severity(log.severity)}"
+    content_tag :div, capture(&block), class: klass
+  end
+
+  def display_message(log)
+    link_to(log.message, log) + '&nbsp'.html_safe +
+    tag_label(I18n.t("chaltron.logs.severity.#{log.severity}"), bootstrap_severity(log.severity))
+  end
+
+  def bootstrap_severity(severity)
+    case severity.to_s
+      when 'info'  then 'primary'
+      when 'error' then 'danger'
+      when 'debug' then 'info'
       else 'default'
     end
-    content_tag :div, capture(&block), class: klass
   end
 end
