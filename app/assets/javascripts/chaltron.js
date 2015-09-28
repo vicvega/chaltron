4e57f391f694516516ffcfd7570ca5704f5e11f6
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2015-09-28 12:13:36 +0200

    add minimized version of external javascript (datatables)

--- app/assets/javascripts/chaltron.js
+++ app/assets/javascripts/chaltron.js
@@ -1,8 +1,8 @@
 //= require bootstrap-sprockets
-//= require dataTables/jquery.dataTables
-//= require dataTables/dataTables.bootstrap
-//= require dataTables/extras/dataTables.responsive
-//= require dataTables/extras/dataTables.select
+//= require dataTables/jquery.dataTables.min
+//= require dataTables/dataTables.bootstrap.min
+//= require dataTables/extras/dataTables.responsive.min
+//= require dataTables/extras/dataTables.select.min
 //= require nprogress
 //= require nprogress-turbolinks
 //= require nprogress-ajax
