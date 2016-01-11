ade4100893078559eba7b85d5ac1ea649f2717cf
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2016-01-11 10:40:24 +0100

    gem update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe User do
-
   context 'validation' do
+    subject { FactoryGirl.build(:user) }
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:username).case_insensitive.scoped_to(:provider) }
+    it { should validate_uniqueness_of(:username).scoped_to(:provider).case_insensitive }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
 
