59df5589ec095c0b2085fb9a84e9457e51d9255c
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2016-07-05 12:28:42 +0200

    fix devise deprecation warning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include LoginHelpers, type: :feature
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
