bf2b17f9d751ec741539d317f2626bb6c2289e26
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2017-12-19 17:36:38 +0100

    update chaltron navbar

--- config/chaltron_navigation.rb
+++ config/chaltron_navigation.rb
@@ -4,30 +4,22 @@ SimpleNavigation::Configuration.run do |navigation|
 #    primary.dom_class = 'pull-right'
 
     if signed_in?
-      primary.item :admin,
-        { icon: 'fa fa-fw fa-cogs',
-          text: I18n.t('chaltron.menu.admin')
-        }, nil do |admin|
-        admin.item :users,
-          { icon: 'fa fa-fw fa-users',
-            text: I18n.t('chaltron.menu.users')
-          }, users_path, highlights_on: /\/(users|ldap)(?!\/self_(show|edit|update))/ if can?(:manage, User)
-        admin.item :logs,
-          { icon: 'fa fa-fw fa-book',
-            text: I18n.t('chaltron.menu.logs')
-          }, logs_path, highlights_on: /\/logs/ if can?(:read, Log)
+      primary.item :admin, { icon: 'cogs',  text: I18n.t('chaltron.menu.admin') }, nil do |admin|
+        admin.item :users, { icon: 'users', text: I18n.t('chaltron.menu.users') },
+          users_path, highlights_on: /\/(users|ldap)(?!\/self_(show|edit|update))/ if can?(:manage, User)
+        admin.item :logs, { icon: 'book', text: I18n.t('chaltron.menu.logs') },
+          logs_path, highlights_on: /\/logs/ if can?(:read, Log)
       end if can?(:manage, User) or can?(:read, Log)
       primary.item :logged, current_user.display_name.html_safe, nil do |user|
         user.dom_class = 'dropdown-menu-right'
-        user.item :self_edit, { icon: 'fa fa-fw fa-user',
+        user.item :self_edit, { icon: 'user',
                   text: I18n.t('chaltron.menu.self_show') }, self_show_users_path,
                   highlights_on: /\/self_(show|edit|update)/
-
-        user.item :logout, { icon: 'fa fa-fw fa-sign-out', text: 'Logout' },
+        user.item :logout, { icon: 'sign-out', text: 'Logout' },
                   destroy_user_session_path, method: :delete
       end
     else
-      primary.item :login, { icon: 'fa fa-fw fa-sign-in', text: 'Login' }, new_user_session_path
+      primary.item :login, { icon: 'sign-in', text: 'Login' }, new_user_session_path
     end
   end
 end
