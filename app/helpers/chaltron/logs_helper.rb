4f14979263bf8ffce4b6e59d7c4a4bc73542111a
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2018-01-18 18:00:54 +0100

    fix show log view

--- app/helpers/chaltron/logs_helper.rb
+++ app/helpers/chaltron/logs_helper.rb
@@ -1,9 +1,4 @@
 module Chaltron::LogsHelper
-  def display_panel(log, &block)
-    klass = "panel panel-#{bootstrap_severity(log.severity)}"
-    content_tag :div, capture(&block), class: klass
-  end
-
   def bootstrap_severity(severity)
     case severity.to_s
     when 'info'  then 'primary'
