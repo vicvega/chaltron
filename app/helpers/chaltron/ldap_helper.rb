8e64690105d4c123f4ffb02beddfd076cd642457
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2018-01-19 16:10:41 +0100

    fix ldap user creation view

--- app/helpers/chaltron/ldap_helper.rb
+++ app/helpers/chaltron/ldap_helper.rb
@@ -3,7 +3,7 @@ module Chaltron::LdapHelper
   def display_entry_name(entry)
     if User.exists?(username: entry.username)
       (entry.name + '&nbsp').html_safe +
-        tag_label(I18n.t('chaltron.users.already_present'), :danger)
+        content_tag(:span, I18n.t('chaltron.users.already_present'), class: 'badge badge-danger')
     else
       entry.name
     end
@@ -12,7 +12,7 @@ module Chaltron::LdapHelper
   def display_entry_email(entry)
     mail = entry.email
     if mail.blank?
-      tag_label(I18n.t('chaltron.users.missing_field'), :danger)
+      content_tag(:span, I18n.t('chaltron.users.missing_field'), class: 'badge badge-danger')
     else
       mail
     end
