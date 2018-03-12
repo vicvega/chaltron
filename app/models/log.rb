8ec07d9357428967af628e76762832dbee100227
Dario Lombardo <lomato@gmail.com>
2018-03-12 12:02:10 +0100

    log: extend severities to support standard syslog's.
    The deprecated (:error, :warn) are supported as well, but they're
    changed on-the-fly to the standard ones.

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,12 +1,20 @@
 class Log < ActiveRecord::Base
-  Severities = %w( info debug error )
+  Severities = %w( emerg panic alert crit err warning notice info debug )
 
   validates_presence_of :severity, :message
   validates_inclusion_of :severity, in: Severities
 
+  before_validation :change_severity
+
   after_create :to_syslog if Chaltron.enable_syslog
 
   private
+
+  def change_severity
+    self.severity = :err if self.severity && self.severity.to_sym == :error
+    self.severity = :warning if self.severity && self.severity.to_sym == :warn
+  end
+
   def to_syslog
     syslog_method =
       case self.severity
