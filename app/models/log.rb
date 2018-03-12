681a9a7dcdb8223f64b471047ed018a4795529cc
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2018-03-12 15:04:44 +0100

    use all standard syslog severity in LogMessage

--- app/models/log.rb
+++ app/models/log.rb
@@ -11,20 +11,13 @@ class Log < ActiveRecord::Base
   private
 
   def change_severity
-    self.severity = :err if self.severity && self.severity.to_sym == :error
+    self.severity = :err     if self.severity && self.severity.to_sym == :error
     self.severity = :warning if self.severity && self.severity.to_sym == :warn
   end
 
   def to_syslog
-    syslog_method =
-      case self.severity
-      when 'info' then :notice
-      when 'debug' then :debug
-      when 'error' then :err
-      else :info
-    end
     Syslog.open(Rails.application.class.parent.to_s, Syslog::LOG_PID, Chaltron.syslog_facility) do |s|
-      s.send(syslog_method, self.category.upcase + ' - ' + self.message)
+      s.send(self.severity.to_sym, self.category.upcase + ' - ' + self.message)
     end
   end
 
