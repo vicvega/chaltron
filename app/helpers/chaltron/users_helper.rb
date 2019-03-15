80a5aba910ec81ffabce2a32ac222cfd98556f15
Gjergji Furxhi <jonny84.gdp@gmail.com>
2019-03-15 10:50:31 +0000

    helpers: fix missing semicolons.
    Fix: #17.

--- app/helpers/chaltron/users_helper.rb
+++ app/helpers/chaltron/users_helper.rb
@@ -1,7 +1,7 @@
 module Chaltron::UsersHelper
   def display_username(user)
     if user == current_user
-      link_to(user.username, user) + '&nbsp'.html_safe +
+      link_to(user.username, user) + '&nbsp;'.html_safe +
         tag_label(I18n.t('chaltron.users.it_s_you'), :success)
     else
       link_to user.username, user
