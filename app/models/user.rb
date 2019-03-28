8c4e6bc18a19f4a5d825281f2835cd17d6c2b68b
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2019-03-28 17:05:07 +0100

    use application_record template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   include Authorizable
   # Include default devise modules. Others available are:
   # :registerable, :confirmable, :lockable and :omniauthable
