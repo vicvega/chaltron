ef9dfc9e7a8803b8fdf7eab6e359398ad584a7db
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2019-03-28 10:13:29 +0100

    add ldap_before_logout callback

--- app/controllers/chaltron/sessions_controller.rb
+++ app/controllers/chaltron/sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'chaltron/ldap/connection'
+
 class Chaltron::SessionsController  < Devise::SessionsController
   after_action :after_login, only: :create
   before_action :before_logout, only: :destroy
@@ -9,6 +11,7 @@ class Chaltron::SessionsController  < Devise::SessionsController
   end
 
   def before_logout
+    Chaltron.ldap_before_logout.call(current_user, Chaltron::LDAP::Connection.new) if current_user.ldap_user?
     info I18n.t('chaltron.logs.logout', user: current_user.display_name)
   end
 end
