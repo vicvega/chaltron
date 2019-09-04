ef9eb9889c393ad263cfb88efa6d685a516f1311
Dario Lombardo <lomato@gmail.com>
2019-09-04 15:41:32 +0200

    bootstrap_form: make inline option programmable.

--- lib/chaltron/bootstrap_form.rb
+++ lib/chaltron/bootstrap_form.rb
@@ -5,7 +5,7 @@ class BootstrapForm::FormBuilder
     checked = @object.nil?? false : @object.roles
     html = inputs_collection(:roles, collection, :first, :last, checked: checked) do |name, value, options|
       options[:multiple] = true
-      options[:inline] = true
+      options[:inline] = opts.fetch(:inline, true)
 
       if value == opts[:disabled]
         options[:disabled] = true
