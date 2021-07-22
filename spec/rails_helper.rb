f0e06e3e89e0058216d51ceff2df65cc0534fa19
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2021-07-22 15:41:52 +0200

    switch to coveralls_reborn gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,19 @@
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
